c60fb270c19913b8453ac6afa291554133e7952a
Pedro H <xtron44@gmail.com>
2024-03-06 23:18:57 -0300

    Trabalhando com exceções

---
+++ Exceptionsss/Exceptionsss/Entities/Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exceptionsss.Entities.Exceptions;
+namespace Exceptionsss.Entities
+{
+    public class Reservation
+    {
+        public int roomNumber {  get; set; }
+        public DateTime checkin {  get; set; }
+
+        public DateTime checkout { get; set; }
+
+        public Reservation(int roomNumber,DateTime checkin,DateTime checkout) { 
+            this.roomNumber = roomNumber;
+
+            if (checkout <= checkin) throw new DomainException("A data de checkout é menor que a data de checkin");
+            else
+            {
+                this.checkin = checkin;
+                this.checkout = checkout;
+            }
+
+        }
+        public int duration()
+        {
+            return (int)checkout.Subtract(checkin).TotalDays;
+        }
+        public void updateDates(DateTime checkin,DateTime checkout)
+        {
+            if ((this.checkin < checkin && this.checkout < checkout) && (checkout > checkin))
+            {
+                this.checkin = checkin;
+                this.checkout = checkout;
+            }
+            else throw new DomainException("Data de atualização não permitida");
+            
+        }
+
+        public override string ToString()
+        {
+            return "Quarto : "+roomNumber+",Checkin : "+checkin+ ",Checkout : "+checkout;
+        }
+    }
+}
---
+++ Exceptionsss/Exceptionsss/Program.cs
@@ -0,0 +1,28 @@
+﻿using Exceptionsss.Entities;
+using Exceptionsss.Entities.Exceptions;
+using System;
+using System.Globalization;
+namespace reservas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Reservation Pedro = new Reservation(1003, DateTime.ParseExact("03/03/2004", "dd/MM/yyyy", CultureInfo.InvariantCulture), DateTime.ParseExact("06/03/2004", "dd/MM/yyyy", CultureInfo.InvariantCulture));
+                Console.WriteLine("Dias : " + Pedro.duration());
+                Console.WriteLine(Pedro.ToString());
+                Pedro.updateDates(DateTime.ParseExact("02/03/2004", "dd/MM/yyyy", CultureInfo.InvariantCulture), DateTime.ParseExact("09/03/2004", "dd/MM/yyyy", CultureInfo.InvariantCulture));
+                Console.WriteLine(Pedro.ToString());
+            }
+            catch(DomainException e) { 
+                Console.WriteLine("Erro na reserva,"+e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro no formato," + e.Message);
+            }
+        }
+    }
+}
