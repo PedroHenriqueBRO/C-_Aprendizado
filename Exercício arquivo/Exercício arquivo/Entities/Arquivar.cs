bab66d64e91bf205e78018d91a571cb920af481e
Pedro H <xtron44@gmail.com>
2024-03-06 23:20:38 -0300

    Exercício manipulando arquivos

---
+++ Exercício arquivo/Exercício arquivo/Entities/Arquivar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_arquivo.Entities
+{
+    public class Arquivar
+    {
+        public string Path { get; set; }
+        public Arquivar(string path)
+        {
+            Path = path;
+        }
+        public void manipulaarquivo(string path)
+        {
+            using (StreamReader fs = new StreamReader(path))
+            {
+                using (StreamWriter sw = new StreamWriter(Path))
+                {
+                    string linhas=fs.ReadToEnd();
+                    string[] linhaseparada = linhas.Split(",");
+                    int cont = 0;
+                    sw.WriteLine("NomeProduto,Preço,Quantidade");
+                    foreach (string sa in linhaseparada)
+                    {
+                        
+                        sw.Write(sa+",");
+                        cont++;
+                        if (cont == 2)
+                        {
+                            cont = 0;
+                            
+                        }else if(cont == 3)
+                        {
+                            sw.WriteLine();
+                        }
+                    }
+
+
+                }
+            }
+            
+        }
+        public void lerarquivo()
+        {
+            using (StreamReader fs = new StreamReader(Path))
+            {
+               
+            }
+        }
+
+    }
+}
---
+++ Exercício arquivo/Exercício arquivo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Exercício_arquivo.Entities;
+using Microsoft.VisualBasic;
+using System;
+using System.IO;
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directorypath = @"D:\arquivos";
+            string path = @"D:\arquivos\aaaa.csv";
+            string newpath= @"D:\arquivos\out\bbbb.csv";
+            
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    
+                    
+                    Directory.CreateDirectory(directorypath + @"\out");
+                    using (StreamWriter sw = File.AppendText(newpath))
+                    {
+                        Arquivar novo =new Arquivar(@"D:\arquivos\out\cccc.csv");
+                        novo.manipulaarquivo(@"D:\arquivos\aaaa.csv");
+                        for (; !sr.EndOfStream;)
+                        {
+                            string s = sr.ReadLine();
+                            string[] infos = s.Split(",");
+                            Console.WriteLine("Nome do produto : " + infos[0]);
+                            Console.WriteLine("Preço total : " + double.Parse(infos[1]) * double.Parse(infos[2]));
+                            sw.WriteLine(infos[0]+","+"Preço total : "+ double.Parse(infos[1]) * double.Parse(infos[2]));
+                        }
+                        
+
+
+                    }
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
